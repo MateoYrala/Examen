db2f3df5d777451746963a661b6b4e011f0ced15
MateoYrala <131172300+MateoYrala@users.noreply.github.com>
2023-09-26 08:50:28 -0500

    Add files via upload

--- practica.js
+++ practica.js
@@ -5,31 +5,31 @@ alert("Bienvenido(a) :"+nom);
 alert("Este cuestionario es acerca de las Olimpiadas escolares, por lo que procederé a preguntarte sobre ellas");
 
 var p1 = prompt("1. ¿Cuándo se realizará la inauguración de las Olimpiadas?\nA. Lunes 02 de octubre\nB. Viernes 29 de setiembre\nC. Jueves 28 de setiembre");
-if (p1 = B){
+if (p1 = "B"){
     c=c+1
-}else if (p1 = A){
+}else if (p1 = "A"){
 i=i+1
-    }else if(p1 = C){
+    }else if(p1 = "C"){
         i=i+1
     }else{
         alert("No es una opción válida")
     }
     var p2 = prompt("2-¿De qué color es la prompoción de 2do de secundaria?\nA. Celeste o Turquesa\nB. Rojo\nC. Morado");
-    if (p2 = A){
+    if (p2 = "A"){
         c=c+1
-    }else if (p2 = B){
+    }else if (p2 = "B"){
     i=i+1
-        }else if(p2 = C){
+        }else if(p2 = "C"){
             i=i+1
         }else{
             alert("No es una opción válida")
         }
         var p3 = prompt("3. Nuevo deporte incluido este año\nA. Futsal femenino\nB. Balonmano\nC. Marcha atlética");
-        if (p3 = A){
+        if (p3 = "A"){
             c=c+1
-        }else if (p3 = B){
+        }else if (p3 = "B"){
         i=i+1
-            }else if(p3 = C){
+            }else if(p3 = "C"){
                 i=i+1
             }else{
                 alert("No es una opción válida")
